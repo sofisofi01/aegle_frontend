badd84baa0677d3eeac6401d65611859db52054e
alissa <alisazimina7@gmail.com>
2025-12-07 01:28:00 +0300

    пока так

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import { HeaderProps } from "./types";
+import styles from "./Header.module.scss";
+import classNames from "classnames";
+
+
+export default function Header({ active }: HeaderProps) {
+const menuItems = [
+{ id: "nutrition", name: "Nutrition" },
+{ id: "workouts", name: "Workouts" },
+{ id: "progress", name: "Progress" },
+];
+
+
+return (
+<header className={styles.header}>
+<div className={styles.logo}>E Q U I L I B R I A</div>
+
+
+<nav className={styles.nav}>
+<ul className={styles.menuList}>
+{menuItems.map((item) => (
+<li
+key={item.id}
+className={classNames(styles.menuItem, {
+[styles.active]: active === item.id,
+})}
+>
+<a href={`/#${item.id}`}>{item.name}</a>
+</li>
+))}
+</ul>
+</nav>
+
+
+<button className={styles.signIn}>Sign in</button>
+</header>
+);
+}
