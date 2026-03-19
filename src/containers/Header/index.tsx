a21c69a2e1494f79bcf6c3701a0ccd58936b101f
alissa <alisazimina7@gmail.com>
2026-03-19 04:15:23 +0300

    header footer

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -10,8 +10,11 @@ export function Header({ menu }: HeaderProps) {
   return (
     <header className={styles.header}>
       <Link href="/" className={styles.logo}>
-        <Image src={logo} alt="Logo" width={60} height={60} className={styles.image} />
-        <h1 className={styles.title}>Equilibria Your Wellness App</h1>
+        <Image src={logo} alt="Logo" width={180} height={180} className={styles.image} />
+        <div className={styles.logoText}>
+          <h1 className={styles.logoMain}>Equilibria</h1>
+          <h2 className={styles.logoSub}>your wellness app</h2>
+        </div>
       </Link>
       <nav className={styles.nav}>
         {menu.map((item) => (
