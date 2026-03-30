9b3b0c3448fcda9e0b68db348fbed7867e38ccd4
alissa <alisazimina7@gmail.com>
2026-03-31 01:55:04 +0300

    main готов

--- src/landings/main/index.tsx
+++ src/landings/main/index.tsx
@@ -50,7 +50,6 @@ export function MainPage() {
         </div>
 
         <div className={styles.advCard}>
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>
           <filter id="blur">
           <feGaussianBlur stdDeviation="6"></feGaussianBlur>
           </filter>
