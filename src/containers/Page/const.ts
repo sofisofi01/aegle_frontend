27e45f6c2e26aac1e607f3f45cd6be8ceffaecab
d_d <d.v.cerveza@gmail.com>
2026-06-05 21:39:05 +0300

    const fix

--- src/containers/Page/const.ts
+++ src/containers/Page/const.ts
@@ -3,5 +3,5 @@ import { MenuItem } from "@/containers/Header/types";
 export const data: MenuItem[] = [
   { id: 1, text: "Питание", href: "/nutrition" },
   { id: 2, text: "Тренировки", href: "/workout" },
-  { id: 4, text: "ИИ консультант", href: "/ai-consultant" },
+  { id: 4, text: "ИИ Консультант", href: "/ai-consultant" },
 ];
